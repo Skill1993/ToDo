c09a6eb1bfb64d52fc52a75647f93099b17000ca
ZNA <skill1993@gmail.com>
2018-10-01 21:29:38 +0300

    Added get started label and copyright label

--- ToDooDaBuDiDoo/WelcomeController.swift
+++ ToDooDaBuDiDoo/WelcomeController.swift
@@ -20,6 +20,16 @@ class WelcomeController: UIViewController {
     
 //    let titleLabel = GDLabel()
     let titleLabel = GDLabel(title: "TO DO DAT", size:24, textAlign: .center)
+//    let infoLabel = GDLabel(title: "WELCOME. TO DO DAT IS A TO DO LIST.\nA REALLY DOPE TO DO LIST", size: 14, textAlign: .center)
+    let infoLabel: UILabel = {
+        let label = GDLabel(title: "WELCOME. TO DO DAT IS A TO DO LIST.\nA REALLY DOPE TO DO LIST", size: 14, textAlign: .center)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    let nextButton = UIButton()
+    
+    let copyrightLabel = GDLabel(title: "© 2018 | NAZ Apps", color: .grayZero, size: 14, textAlign: .center)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +44,27 @@ class WelcomeController: UIViewController {
         bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
         
-        view.addSubview(titleLabel)
+        bg.addSubview(titleLabel)
         titleLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
         titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
         titleLabel.topAnchor.constraint(equalTo: bg.topAnchor, constant: 60).isActive = true
-
+        
+        bg.addSubview(infoLabel)
+//        infoLabel.numberOfLines = 2
+        infoLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
+        infoLabel.centerYAnchor.constraint(equalTo: bg.centerYAnchor).isActive = true
+        infoLabel.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        infoLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        bg.addSubview(nextButton)
+        
+        view.addSubview(copyrightLabel)
+        copyrightLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        copyrightLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        copyrightLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        copyrightLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        
     }
 
 
