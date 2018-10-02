70a262cef9ffa630bd8d5516bcc5fd80b607c47f
ZNA <skill1993@gmail.com>
2018-10-03 00:41:55 +0300

    added a cancel button

--- ToDooDaBuDiDoo/custom/NewItemPopUp.swift
+++ ToDooDaBuDiDoo/custom/NewItemPopUp.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class NewItemPopUp: GDGradient {
+    
+    let cancel = GDButton(title: " cancel ", type: .roundedText, radius: 4)
+    
     override init(frame: CGRect = .zero){
         super.init(frame: frame)
         
+        let inset:CGFloat = 16
+        
         self.layer.cornerRadius = 14
+        
+        addSubview(cancel)
+        cancel.leftAnchor.constraint(equalTo: leftAnchor, constant: inset).isActive = true
+        cancel.topAnchor.constraint(equalTo: topAnchor, constant: inset).isActive = true
+        cancel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
