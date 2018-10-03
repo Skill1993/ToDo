3fa289f88254b81c4ebe286762fd361c3c1a3d12
ZNA <skill1993@gmail.com>
2018-10-03 17:24:53 +0300

    First steps on our table view

--- ToDooDaBuDiDoo/controllers/ListController.swift
+++ ToDooDaBuDiDoo/controllers/ListController.swift
@@ -21,6 +21,13 @@ class ListController: UIViewController, GDHeaderDelegate, GDNewItemDelegate {
     let header = GDHeaderView(title: "Stuff to get done", subtitle: "4 left")
     let popup = GDNewItemPopUp()
     
+    let bg: UIView = {
+        let view = GDGradient()
+        view.backgroundColor = .cyan
+        view.layer.cornerRadius = 24
+        return view
+    }()
+    
     var keyboardHeight: CGFloat = 260
     
     override func viewDidAppear(_ animated: Bool) {
@@ -37,6 +44,7 @@ class ListController: UIViewController, GDHeaderDelegate, GDNewItemDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
         
         view.addSubview(header)
@@ -44,6 +52,12 @@ class ListController: UIViewController, GDHeaderDelegate, GDNewItemDelegate {
         header.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         header.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         header.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        view.addSubview(bg)
+        bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        bg.topAnchor.constraint(equalTo: header.bottomAnchor, constant: 20).isActive = true
+        bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
         
         view.addSubview(popup)
         popup.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
---
+++ ToDooDaBuDiDoo/custom/GDTableView.swift
@@ -0,0 +1,19 @@
+//
+//  GDTableView.swift
+//  ToDooDaBuDiDoo
+//
+//  Created by Никита Журавлев on 03/10/2018.
+//  Copyright © 2018 Никита Журавлев. All rights reserved.
+//
+
+import UIKit
+
+class GDTableView: UITableView {
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
